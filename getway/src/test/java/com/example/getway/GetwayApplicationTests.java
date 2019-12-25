5d0492677595729694c55fee4dbabe19b0524a1b
WANGXU-SU <18656701032@sina.cn>
2019-12-25 12:51:29 +0800

    wx

---
+++ getway/src/test/java/com/example/getway/GetwayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.getway;
+
+import org.testng.annotations.AfterTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class GetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
